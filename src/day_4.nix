d6717038eb52386a7e0b23cd680880c59abb7bb1
nuexq <nuexqq@gmail.com>
2025-12-05 17:19:49 +0100

    feat: day 4 part 2

--- src/day_4.nix
+++ src/day_4.nix
@@ -1,7 +1,7 @@
 let
   lib = import <nixpkgs/lib>;
   elemAt = builtins.elemAt;
-  inherit (lib) map genList;
+  inherit (lib) count;
 
   input = ''
     ..@@.@@@@.
@@ -18,30 +18,52 @@ let
 
   cleanedLines = builtins.filter (s: s != "")
     (map lib.strings.trim (lib.strings.splitString "\n" input));
-  grid = map (line: lib.strings.stringToCharacters line) cleanedLines;
 
-  colsNum = builtins.length (elemAt grid 0);
-  rowsNum = builtins.length grid;
+  initialGrid = map (line: lib.strings.stringToCharacters line) cleanedLines;
+
+  colsNum = builtins.length (elemAt initialGrid 0);
+  rowsNum = builtins.length initialGrid;
 
   inBounds = r: c: r >= 0 && r < rowsNum && c >= 0 && c < colsNum;
-  get = r: c: if inBounds r c then elemAt (elemAt grid r) c else ".";
+  get = r: c: grid: if inBounds r c then elemAt (elemAt grid r) c else ".";
 
-  allPositions = builtins.concatLists
-    (genList (r: genList (c: { inherit r c; }) colsNum) rowsNum);
-
-  part1 = builtins.length (builtins.concatMap ({ r, c }:
+  solution = grid:
     let
-      cell = get r c;
-      neighbors = lib.count (x: x == "@") [
-        (get (r - 1) (c - 1))
-        (get (r - 1) c)
-        (get (r - 1) (c + 1))
-        (get r (c - 1))
-        (get r (c + 1))
-        (get (r + 1) (c - 1))
-        (get (r + 1) c)
-        (get (r + 1) (c + 1))
-      ];
-    in if cell == "@" && neighbors < 4 then [ 1 ] else [ ]) allPositions);
-in builtins.trace "Part 1" part1
+      result = builtins.genList (r:
+        builtins.genList (c:
+          let
+            cell = get r c grid;
+            neighbors = count (x: x == "@") [
+              (get (r - 1) (c - 1) grid)
+              (get (r - 1) c grid)
+              (get (r - 1) (c + 1) grid)
+              (get r (c - 1) grid)
+              (get r (c + 1) grid)
+              (get (r + 1) (c - 1) grid)
+              (get (r + 1) c grid)
+              (get (r + 1) (c + 1) grid)
+            ];
+            died = cell == "@" && neighbors < 4;
+            newCell = if died then "." else cell;
+          in { inherit newCell died; }) colsNum) rowsNum;
+
+      newGrid = map (row: map (x: x.newCell) row) result;
+
+      deaths = builtins.foldl'
+        (acc: row: acc + builtins.length (builtins.filter (x: x.died) row)) 0
+        result;
+
+    in { inherit newGrid deaths; };
+
+  totalDeaths = grid:
+    let step = solution grid;
+    in if grid == step.newGrid then
+      step.deaths
+    else
+      step.deaths + (totalDeaths step.newGrid);
+
+  part1 = (solution initialGrid).deaths;
+  part2 = totalDeaths initialGrid;
+
+in { inherit part1 part2; }
 
